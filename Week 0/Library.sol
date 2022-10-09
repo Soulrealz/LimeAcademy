80f647be5741d79a70f2c0db8a9d16b2d002345c
Soulrealz <breekzstalkers@gmail.com>
2022-10-09 14:34:44 +0300

    created Library.sol and added functionality for adding/borrowing/returning books

--- Week 0/Library.sol
+++ Week 0/Library.sol
@@ -1,5 +1,101 @@
 pragma solidity ^0.8.17;
 
 contract Library {
-    
+    struct Book { 
+        uint bookId;
+        uint availableCopies;
+        string bookName;
+        address[] allBorrowers;      
+    }
+
+    address private __owner;
+    mapping(uint => Book) private __idToBook;
+    mapping(uint => mapping(address => bool)) bookAddressesCurrentlyBorrowing;
+    uint[] private __bookIds;
+
+    uint public countOfAvailableBooks;
+
+    error InvalidBookIndex();
+    error Unauthorized();
+    error NoCopiesAvailable();
+    error CannotBorrowMoreCopies();
+    error NoCopiesOwned();
+
+    modifier onlyOwner {
+        if (msg.sender != __owner) {
+            revert Unauthorized();
+        }
+        _;
+    }
+
+    modifier onlyExistingIndex(uint _bookId) {
+        if (__idToBook[_bookId].bookId == 0) {
+            revert InvalidBookIndex(); 
+        }
+        _;
+    }
+
+    constructor() {
+        __owner = msg.sender;
+        countOfAvailableBooks = 0;
+    }
+
+    function addNewBooks(uint _bookId, string calldata _bookName, uint _copies) public onlyOwner onlyExistingIndex(_bookId) {
+        require(_copies > 0, "Cannot add books with zero copies.");
+
+        address[] memory empty;
+        __idToBook[_bookId] = Book(_bookId, _copies, _bookName, empty);
+        __bookIds.push(_bookId);
+        countOfAvailableBooks++;
+    }
+
+    function addCopiesToExistingBook(uint _bookId, uint _copies) public onlyOwner onlyExistingIndex(_bookId) {
+        require(_copies > 0, "Cannot add books with zero copies.");
+        if (__idToBook[_bookId].availableCopies == 0) {
+            countOfAvailableBooks++;
+        }
+
+        __idToBook[_bookId].availableCopies += _copies;
+    }
+
+    function getAllAvailableBooks() public view returns(Book[] memory available) {
+        available = new Book[](countOfAvailableBooks);
+        uint availableCounter = 0;
+        for (uint i = 0; i < __bookIds.length; ++i) {
+            Book memory book = __idToBook[__bookIds[i]];
+            if (book.availableCopies > 0) {
+                available[availableCounter++] = book;
+            }
+        }
+    }
+
+    function borrowBook(uint _bookId) external onlyExistingIndex(_bookId) returns(Book memory desiredBook) {
+        if (bookAddressesCurrentlyBorrowing[_bookId][msg.sender] == true) {
+            revert CannotBorrowMoreCopies();
+        }
+
+        if (__idToBook[_bookId].availableCopies <= 0) {
+            revert NoCopiesAvailable();
+        }
+
+        desiredBook = __idToBook[_bookId];
+        __idToBook[_bookId].availableCopies--;
+        if (__idToBook[_bookId].availableCopies == 0) {
+            countOfAvailableBooks--;
+        }
+        __idToBook[_bookId].allBorrowers.push(msg.sender);
+        bookAddressesCurrentlyBorrowing[_bookId][msg.sender] == true;
+    }
+
+    function returnBook(Book calldata book) external onlyExistingIndex(book.bookId) {
+        if (bookAddressesCurrentlyBorrowing[book.bookId][msg.sender] == false) {
+            revert NoCopiesOwned();
+        }
+
+        __idToBook[book.bookId].availableCopies++;
+        if (__idToBook[book.bookId].availableCopies == 1) {
+            countOfAvailableBooks++;
+        }
+        bookAddressesCurrentlyBorrowing[book.bookId][msg.sender] = false;
+    }
 }
